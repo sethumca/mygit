ba19f1077bd541c6f615f768c4709d1a70e3c65d
sethumca <cjsethupandian@gmail.com>
2018-01-13 16:22:02 +0530

    Person.java

---
+++ Person.java
@@ -0,0 +1,44 @@
+package extra;
+
+public class Person {
+
+	private String fullName;
+	private String gender;
+	private boolean single;
+
+	public Person(String fullName, String gender, boolean single) {
+		this.fullName = fullName;
+		this.gender = gender;
+		this.single = single;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public boolean isSingle() {
+		return single;
+	}
+
+	public void setSingle(boolean single) {
+		this.single = single;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [fullName=" + fullName + ", gender=" + gender + ", single=" + single + "]";
+	}
+	
+}
